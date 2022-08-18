4915d0619fbac516e9a5f3826bb7f1322d8f08b8
Ragib Badaruddin <ragib.badaruddin@gmail.com>
2022-08-18 08:59:21 +0800

    add mui react

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
+import CssBaseline from '@mui/material/CssBaseline';
 import type { AppProps } from 'next/app'
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <CssBaseline/>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
