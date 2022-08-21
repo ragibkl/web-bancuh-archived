843b50dd69391349815556c3bca8bdcc680e397c
Ragib Badaruddin <ragib.badaruddin@gmail.com>
2022-08-21 17:58:27 +0800

    header nav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,9 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+import GitHubIcon from "@mui/icons-material/GitHub";
 import Link from "next/link";
+import IconButton from "@mui/material/IconButton";
 
 export default function Header() {
   return (
@@ -11,11 +13,23 @@ export default function Header() {
       <AppBar position="static">
         <Toolbar>
           <Link href="/">
-            <Button color="inherit" href="">
+            <Button color="inherit">
               <Typography color="inherit" variant="h6" sx={{ flexGrow: 1 }}>
                 Bancuh DNS
               </Typography>
             </Button>
+          </Link>
+
+          <Box sx={{ flex: 1 }} />
+
+          <Link href="/get-started">
+            <Button color="inherit">Get Started</Button>
+          </Link>
+
+          <Link href="https://github.com/ragibkl/adblock-dns-server">
+            <IconButton>
+              <GitHubIcon />
+            </IconButton>
           </Link>
         </Toolbar>
       </AppBar>
